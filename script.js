523c71cf87ec44c9da2d22ef272e52d7819caf1c
バイナリ <wills_pride.6c@icloud.com>
2026-01-19 21:13:54 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll(".scroll-section");
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting){
+          entry.target.classList.add("show");
+        }
+      });
+    }, { threshold: 0.2 });
+
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  });
+</script>
